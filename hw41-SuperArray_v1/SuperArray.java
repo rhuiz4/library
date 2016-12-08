d50f38a659fe55428333f80d9357906eca224c63
tofr <mykolyk@stuycs.org>
2016-12-08 08:45:22 -0500

    add SuperArray v1

---
+++ hw41-SuperArray_v1/SuperArray.java
@@ -0,0 +1,91 @@
+/*==================================================
+  class SuperArray version 1.0
+  Wrapper class for array. Facilitates resizing, 
+  expansion, and read/write capability on elements.
+  ==================================================*/
+
+public class SuperArray 
+{
+    private int[] _data;  //underlying container structure
+    private int _lastPos; //marker for last meaningful value
+    private int _size;    //number of meaingful values
+
+    //default constructor
+    //initializes 10-item array
+    public SuperArray() 
+    { 
+	_data = new int[10];
+	_lastPos = -1;
+	_size = 0;	
+    }
+
+
+    //output array in [a,b,c] format
+    //eg, for int[] a = {1,2,3} ...
+    //toString() -> "[1,2,3]"
+    public String toString() 
+    { 
+	String foo = "[";
+	for( int i = 0; i < _size; i++ ) {
+	    foo += _data[i] + ",";
+	}
+	if ( foo.length() > 1 )
+	    foo = foo.substring( 0, foo.length()-1 );
+	foo += "]";
+	return foo;
+    }
+
+
+    //double capacity of this instance of SuperArray 
+    private void expand() 
+    { 
+	int[] temp = new int[ _data.length * 2 ];
+	for( int i = 0; i < _data.length; i++ )
+	    temp[i] = _data[i];
+	_data = temp;
+    }
+
+
+    //accessor method -- return value at specified index
+    public int get( int index ) 
+    {
+	return _data[index];
+    }
+
+
+    //mutator method -- set index to newVal, return old value at index
+    public int set( int index, int newVal ) 
+    {
+ 	int temp = _data[index];
+	_data[index] = newVal;
+	return temp;
+    }
+
+
+    //main method for testing
+    public static void main( String[] args ) 
+    {
+
+	SuperArray curtis = new SuperArray();
+	System.out.println( "Printing empty SuperArray curtis..." );
+	System.out.println( curtis );
+
+	for( int i = 0; i < curtis._data.length; i++ ) {
+	    curtis.set( i, i * 2 );
+	}
+
+	System.out.println("Printing populated SuperArray curtis...");
+	System.out.println(curtis);
+
+	for( int i = 0; i < 3; i++ ) {
+	    curtis.expand();
+	    System.out.println("Printing expanded SuperArray curtis...");
+	    System.out.println(curtis);
+	    System.out.println("new length of underlying array: " 
+			       + curtis._data.length );
+	}
+	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }//end main()
+
+}//end class SuperArray
